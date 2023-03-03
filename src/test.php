ab428dc8fd1eef1aa5c8d8f5b2e4ee8c5b1edfc8
daOrkz <fillippok87@gmail.com>
2023-03-03 20:42:21 +0700

    test token

--- src/test.php
+++ src/test.php
@@ -5,23 +5,3 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
-//var_dump(empty($expected_array_got_string['some_key']));
-//var_dump(empty($expected_array_got_string[0]));
-//var_dump(empty($expected_array_got_string['0']));
-//var_dump(empty($expected_array_got_string[0.5]));
-//var_dump(empty($expected_array_got_string['0.5']));
-//var_dump(empty($expected_array_got_string['0 Mostel']));
-
-$expected_array_got_string = ['0 Mostel'];
-var_dump(empty($expected_array_got_string));
-$expected_array_got_string = [0];
-var_dump(empty($expected_array_got_string));
-$expected_array_got_string = ['0'];
-var_dump(empty($expected_array_got_string));
-$expected_array_got_string = [0.5];
-var_dump(empty($expected_array_got_string));
-$expected_array_got_string = ['0.5'];
-var_dump(empty($expected_array_got_string));
-$expected_array_got_string = ['0 Mostel'];
-
-var_dump(empty($expected_array_got_string));
